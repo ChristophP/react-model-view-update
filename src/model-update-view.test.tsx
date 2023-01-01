7bc37fdfea2c04212df93814c2d434b9ddb840d5
ChristophP <deedop@hotmail.de>
2023-01-01 21:02:26 +0100

    get tests to type check 🚀

--- src/model-update-view.test.tsx
+++ src/model-update-view.test.tsx
@@ -1,6 +1,13 @@
 import { render, fireEvent, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { createApp, useSendMsg, Implementation, Sub, Effect, SendMsgFn } from "./model-update-view";
+import {
+  createApp,
+  useSendMsg,
+  Implementation,
+  Sub,
+  Effect,
+  SendMsgFn,
+} from "./model-update-view";
 
 function ResetButton() {
   const sendMsg = useSendMsg();
@@ -14,7 +21,7 @@ function ResetButton() {
 type Model = number;
 type Msg = { type: string }; // TODO improve Msg type
 
-const impl : Implementation<Model, Msg>  = {
+const impl: Implementation<Model, Msg> = {
   init() {
     return 0;
   },
@@ -87,7 +94,7 @@ describe("update", () => {
 
 describe("effects", () => {
   test("runs an effect returned from the update function", () => {
-    const effectFn = jest.fn() as Effect<Msg>;
+    const effectFn = jest.fn();
     const App = createApp({
       ...impl,
       update(msg, model) {
@@ -118,108 +125,95 @@ describe("effects", () => {
   });
 });
 
-// describe("subscriptions", () => {
-//   function documentClickSubscription(sendMsg : SendMsgFn) : Sub<Msg> {
-//     const listener = () => sendMsg({ type: "documentClick" });
-//     document.addEventListener("click", listener);
-//     return () => {
-//       // return unsubscribe function
-//       document.removeEventListener("click", listener);
-//     };
-//   }
-//
-//   const eventTarget = new EventTarget();
-//   function manualTriggerSubscription(sendMsg) {
-//     const listener = (event) => {
-//       sendMsg(event.detail);
-//     };
-//     eventTarget.addEventListener("trigger", listener);
-//     return () => {
-//       eventTarget.removeEventListener("trigger", listener);
-//     };
-//   }
-//
-//   test("binds and unbinds subscriptions correctly", () => {
-//     const App = createApp({
-//       ...impl,
-//       subscriptions(model) {
-//         return model < 5 ? [documentClickSubscription] : [];
-//       },
-//     });
-//     render(<App />);
-//
-//     fireEvent.click(screen.getByText("+"));
-//     fireEvent.click(screen.getByText("+"));
-//     expect(screen.getByRole("heading")).toHaveTextContent("7");
-//   });
-//
-//   test("unbinds all subscriptions when component unmounts", () => {
-//     const update = jest.fn((model) => [model + 1, []]);
-//     const App = createApp({
-//       ...impl,
-//       update,
-//       subscriptions() {
-//         return [documentClickSubscription];
-//       },
-//     });
-//     const { unmount } = render(<App />);
-//     expect(update).toHaveBeenCalledTimes(0);
-//
-//     fireEvent.click(document.body);
-//     expect(update).toHaveBeenCalledTimes(1);
-//
-//     unmount();
-//     fireEvent.click(document.body);
-//     expect(update).toHaveBeenCalledTimes(1);
-//   });
-//
-//   test("allows creating a manual trigger for subscriptions", () => {
-//     const update = jest.fn((msg, model) => [model, []]);
-//     const App = createApp({
-//       ...impl,
-//       update,
-//       subscriptions() {
-//         return [manualTriggerSubscription];
-//       },
-//     });
-//     render(<App />);
-//
-//     const customTrigger = () =>
-//       eventTarget.dispatchEvent(
-//         new CustomEvent("trigger", { detail: { type: "trigger" } })
-//       );
-//
-//     act(customTrigger);
-//     act(customTrigger);
-//     act(customTrigger);
-//
-//     expect(update).toHaveBeenCalledTimes(3);
-//   });
-//
-//   test("non-function subscriptions will be treated as non-existing and unmounted", () => {
-//     const update = jest.fn((msg, model) => [model + 1, []]);
-//     const App = createApp({
-//       ...impl,
-//       update,
-//       subscriptions(model) {
-//         return [model === 0 ? manualTriggerSubscription : null];
-//       },
-//     });
-//     render(<App />);
-//
-//     const customTrigger = () =>
-//       eventTarget.dispatchEvent(
-//         new CustomEvent("trigger", { detail: { type: "trigger" } })
-//       );
-//     act(customTrigger);
-//     expect(update).toHaveBeenCalledTimes(1);
-//     act(customTrigger);
-//     expect(update).toHaveBeenCalledTimes(1);
-//   });
-// });
-//
+describe("subscriptions", () => {
+  const documentClickSubscription: Sub<Msg> = (sendMsg) => {
+    const listener = () => {
+      sendMsg({ type: "documentClick" });
+    };
+    document.addEventListener("click", listener);
+    return () => {
+      document.removeEventListener("click", listener);
+    };
+  };
+
+  const eventTarget = new EventTarget();
+  const manualTriggerSubscription: Sub<Msg> = (sendMsg) => {
+    const listener = (event: Event) => {
+      sendMsg((event as CustomEvent).detail);
+    };
+    eventTarget.addEventListener("trigger", listener);
+    return () => {
+      eventTarget.removeEventListener("trigger", listener);
+    };
+  };
+
+  test("binds and unbinds subscriptions correctly", () => {
+    const App = createApp({
+      ...impl,
+      subscriptions(model) {
+        return model < 5 ? [documentClickSubscription] : [];
+      },
+    });
+    render(<App />);
+
+    fireEvent.click(screen.getByText("+"));
+    fireEvent.click(screen.getByText("+"));
+    expect(screen.getByRole("heading")).toHaveTextContent("7");
+  });
+
+  test("unbinds all subscriptions when component unmounts", () => {
+    const update = jest.fn((msg: Msg, model: Model): [Model, Effect<Msg>[]] => [
+      model + 1,
+      [],
+    ]);
+    const App = createApp({
+      ...impl,
+      update,
+      subscriptions() {
+        return [documentClickSubscription];
+      },
+    });
+    const { unmount } = render(<App />);
+    expect(update).toHaveBeenCalledTimes(0);
+
+    fireEvent.click(document.body);
+    expect(update).toHaveBeenCalledTimes(1);
+
+    unmount();
+    fireEvent.click(document.body);
+    expect(update).toHaveBeenCalledTimes(1);
+  });
+
+  test("allows creating a manual trigger for subscriptions", () => {
+    const update = jest.fn((msg, model): [Model, Effect<Msg>[]] => [model, []]);
+    const App = createApp({
+      ...impl,
+      update,
+      subscriptions() {
+        return [manualTriggerSubscription];
+      },
+    });
+    render(<App />);
+
+    const customTrigger = () =>
+      eventTarget.dispatchEvent(
+        new CustomEvent("trigger", { detail: { type: "trigger" } })
+      );
+
+    act(customTrigger);
+    act(customTrigger);
+    act(customTrigger);
+
+    expect(update).toHaveBeenCalledTimes(3);
+  });
+});
+
 describe("useSendMsg", () => {
-  function SubComponent({ originalSendMsg } : { originalSendMsg : SendMsgFn<Msg>}) {
+  function SubComponent({
+    originalSendMsg,
+  }: {
+    originalSendMsg: SendMsgFn<Msg>;
+  }) {
     const sendMsg = useSendMsg();
     return originalSendMsg === sendMsg ? <h2>Same</h2> : null;
   }
